dade73fa7da2d8a8a4b44611a28c4f2273c45630
bryancsinformatica <bryancsinformatica@gmail.com>
2024-04-14 05:12:27 -0600

    refactoring

--- app/src/main/java/com/binarybrains/movit/MainActivity.kt
+++ app/src/main/java/com/binarybrains/movit/MainActivity.kt
@@ -8,9 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.binarybrains.movit.register.ui.RegisterScreen
-import com.binarybrains.movit.view.login.ui.LoginScreen
 import com.binarybrains.movit.ui.theme.MovitTheme
-import com.binarybrains.movit.view.login.ui.LoginViewModel
 import com.binarybrains.movit.view.register.ui.RegisterVIewModel
 
 class MainActivity : ComponentActivity() {
