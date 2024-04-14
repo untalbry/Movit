1636b36a95e4e997ae8b276a5919ae7c2d541751
bryancsinformatica <bryancsinformatica@gmail.com>
2024-04-13 23:40:13 -0600

    fix name for enums

--- app/src/main/java/com/binarybrains/movit/utils/enums/MaxCreditEnum.kt
+++ app/src/main/java/com/binarybrains/movit/utils/enums/MaxCreditEnum.kt
@@ -6,7 +6,7 @@ enum class MaxCreditEnum (val id: Int, val credit: Double){
     THIRTH_CREDIT(2, 1000.0),
     FORTH_CREDIT(3, 2000.0),
     FIFTH_CREDIT(4, 3000.0),
-    SIX_CREDIT(5,5000.0)
+    SIXTH_CREDIT(5,5000.0)
 }
 fun getCreditValueById(id: Int): Double?{
     return MaxCreditEnum.entries.find { it.id == id }?.credit
