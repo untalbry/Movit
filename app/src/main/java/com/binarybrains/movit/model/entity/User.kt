3c476159a7054c0e2572311a5f06ccb35a576ab0
bryancsinformatica <bryancsinformatica@gmail.com>
2024-04-13 22:02:49 -0600

    fix user entity class

--- app/src/main/java/com/binarybrains/movit/model/entity/User.kt
+++ app/src/main/java/com/binarybrains/movit/model/entity/User.kt
@@ -4,24 +4,21 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
-
-class User {
-    @PrimaryKey(autoGenerate = true)
-    val user_id: Long = 0
-    @ColumnInfo(name = "first_name") val firstName: String = ""
-    @ColumnInfo(name = "last_name") val lastName: String = ""
-    @ColumnInfo(name = "email") val email: String = ""
-    @ColumnInfo(name = "password")  val password : String = ""
-    @ColumnInfo(name = "income") val incom : Double = 0.0
-    @ColumnInfo(name = "curp") val curp : String = ""
-    @ColumnInfo(name = "age") val age: Int = 0
-    @ColumnInfo(name = "geneder") val gender : String = ""
-    @ColumnInfo(name = "ocupation") val ocupation : String = ""
-    @ColumnInfo(name = "address") val address :String = ""
-    @ColumnInfo(name = "ine") val ine : String = ""
-    @ColumnInfo(name = "phone") val phone : String = ""
-    @ColumnInfo(name = "bills") val bills : Double = 0.0
-    @ColumnInfo(name = "availability") val availability : String = ""
-
-}
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey(autoGenerate = true) val user_id: Long = 0,
+    @ColumnInfo(name = "first_name") val firstName: String?,
+    @ColumnInfo(name = "last_name") val lastName: String?,
+    @ColumnInfo(name = "email") val email: String?,
+    @ColumnInfo(name = "password") val password: String?,
+    @ColumnInfo(name = "income") val income: Double?,
+    @ColumnInfo(name = "curp") val curp: String?,
+    @ColumnInfo(name = "age") val age: Int?,
+    @ColumnInfo(name = "gender") val gender: String?,
+    @ColumnInfo(name = "occupation") val occupation: String?,
+    @ColumnInfo(name = "address") val address: String?,
+    @ColumnInfo(name = "ine") val ine: String?,
+    @ColumnInfo(name = "phone") val phone: String?,
+    @ColumnInfo(name = "bills") val bills: Double?,
+    @ColumnInfo(name = "availability") val availability: String?
+)
