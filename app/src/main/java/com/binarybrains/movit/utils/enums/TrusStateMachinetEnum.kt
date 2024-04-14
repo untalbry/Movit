a60a4a7ec9dbfd9ab7f2c8f8f6433236a0023a87
bryancsinformatica <bryancsinformatica@gmail.com>
2024-04-13 20:54:35 -0600

    add state machines enums

---
+++ app/src/main/java/com/binarybrains/movit/utils/enums/TrusStateMachinetEnum.kt
@@ -0,0 +1,9 @@
+package com.binarybrains.movit.utils.enums
+
+enum class TrusStateMachinetEnum (val index: Int){
+    FIRST_LEVEL(0),
+    SECOND_LEVEL(1),
+    THIRTH_LEVEL(2),
+    FOURTH_LEVEL(3),
+    FIFTH_LEVEL(4)
+}
