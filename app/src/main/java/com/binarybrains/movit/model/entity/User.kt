3102818d08129e4c05e065074d96715e719836b6
bryancsinformatica <bryancsinformatica@gmail.com>
2024-04-13 20:29:19 -0600

    add(user entity)

---
+++ app/src/main/java/com/binarybrains/movit/model/entity/User.kt
@@ -0,0 +1,23 @@
+package com.binarybrains.movit.model.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+
+class User {
+    @PrimaryKey(autoGenerate = true)
+    val user_id: Long = 0
+    @ColumnInfo(name = "first_name") val firstName: String = ""
+    @ColumnInfo(name = "last_name") val lastName: String = ""
+    @ColumnInfo(name = "email") val email: String = ""
+    @ColumnInfo(name = "password")  val password : String = ""
+    @ColumnInfo(name = "income") val incom : Double = 0.0
+    @ColumnInfo(name = "curp") val curp : String = ""
+    @ColumnInfo(name = "age") val age: Int = 0
+    @ColumnInfo(name = "geneder") val gender : String = ""
+    @ColumnInfo(name = "ocupation") val ocupation : String = ""
+
+
+}
